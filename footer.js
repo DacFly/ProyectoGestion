8c2812675adea81f5ee026f028dbef51ad84e4bb
Jimenez29 <Doryanjimenez2903@hotmail.com>
2023-06-20 02:30:41 -0600

    Agregar Evento

--- footer.js
+++ footer.js
@@ -1,28 +1,41 @@
 var contenido = document.querySelector("#footer");
 
-contenido.innerHTML +=` <footer class="bg-dark text-center text-white fixed-bottom">
-<!-- Grid container -->
-<div class="container-fluid ">
-  <!-- Section: Social media -->
-  
-  <!-- Section: Social media -->
-</div>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-6">
-      <a class="text-white" href="" >Eventos</a>
+contenido.innerHTML += `
+  <footer class="bg-dark text-center text-white pt-4">
+    <!-- Grid container -->
+    <div class="container-fluid">
+      <!-- Section: Social media -->
+      <div class="row">
+        <div class="col">
+          <a href="#" class="text-white me-3">
+            <i class="bi bi-facebook fs-3"></i>
+          </a>
+          <a href="#" class="text-white me-3">
+            <i class="bi bi-twitter fs-3"></i>
+          </a>
+          <a href="#" class="text-white me-3">
+            <i class="bi bi-instagram fs-3"></i>
+          </a>
+        </div>
+      </div>
+      <!-- Section: Social media -->
     </div>
-    <div class="col-6">
-      <a class="text-white" href="">Cerrar Sesion</a>
-    </div>
- 
-  </div>
-</div>
 
-<!-- Copyright -->
-<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-  © 2023 SistemaEventos:
-  <a class="text-white" href="">SistemaEventos.com</a>
-</div>
-<!-- Copyright -->
-</footer>`
+    <div class="container-fluid mt-4">
+      <div class="row">
+        <div class="col-6">
+          <a class="text-white" href="">Eventos</a>
+        </div>
+        <div class="col-6">
+          <a class="text-white" href="">Cerrar Sesion</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center py-3" style="background-color: rgba(0, 0, 0, 0.6);">
+      © 2023 SistemaEventos:
+      <a class="text-white" href="">SistemaEventos.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>`;

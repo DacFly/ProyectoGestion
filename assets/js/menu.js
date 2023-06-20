24db797f6baad5e7fdf1434d27d182b66155765e
WillSG-13 <Wilmarsequeira13@gmail.com>
2023-06-20 04:24:44 -0600

    w

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -10,7 +10,7 @@ contenido.innerHTML +=`<nav class="navbar navbar-expand-lg navbar-dark bg-dark "
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" >
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <a
           class="nav-link" id="Inicio"

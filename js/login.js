fc4bd518e6abdfaf1972cd9d84e0ede80ce7ce83
WillSG-13 <Wilmarsequeira13@gmail.com>
2023-06-20 17:45:51 -0600

    s

--- js/login.js
+++ js/login.js
@@ -4,17 +4,15 @@ var btn = document.getElementById('btn');
 
 
 function logueo(){
-    e.preventDefault();
             // Datos del formulario
             var correo =document.getElementById("user").value;
             var contrasena = document.getElementById("pass").value;
 console.log(correo+""+contrasena);
             // Objeto de configuración para la solicitud 
-    fetch("https://localhost:7088/Usuario/Logi/",
+    fetch("https://localhost:7088/Usuario/Login/"+correo+"/"+contrasena,
     {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ correo: correo, contrasena: contrasena })
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' }
     })
     .then(respuesta => {
         if (respuesta.status == 200) {

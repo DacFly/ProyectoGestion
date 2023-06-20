2f1b17c62cd4536247dc3d1bcbaece50faee811c
Jimenez29 <Doryanjimenez2903@hotmail.com>
2023-06-20 00:35:35 -0600

    footer

---
+++ footer.js
@@ -0,0 +1,28 @@
+var contenido = document.querySelector("#footer");
+
+contenido.innerHTML +=` <footer class="bg-dark text-center text-white">
+<!-- Grid container -->
+<div class="container p-4 pb-0">
+  <!-- Section: Social media -->
+  
+  <!-- Section: Social media -->
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-6">
+      <a class="text-white" href="" >Eventos</a>
+    </div>
+    <div class="col-6">
+      <a class="text-white" href="">Cerrar Sesion</a>
+    </div>
+ 
+  </div>
+</div>
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  © 2023 SistemaEventos:
+  <a class="text-white" href="">SistemaEventos.com</a>
+</div>
+<!-- Copyright -->
+</footer>`

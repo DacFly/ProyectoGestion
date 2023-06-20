0d8c7bfcc3960d3289537a6a6d0f287f676800d7
WillSG-13 <Wilmarsequeira13@gmail.com>
2023-06-20 16:29:51 -0600

    hj

--- js/login.js
+++ js/login.js
@@ -5,15 +5,12 @@ var btn = document.getElementById('btn');
 
 function logueo(){
             // Datos del formulario
-            var correo =document.getElementById("user").value;
-            var contrasena = document.getElementById("pass").value;
 
-            fetch("https://localhost:7088/Evento/listaEventos")
+            fetch("https://localhost:7088/Usuario/listaUsuario")
             .then(respuesta => respuesta.json())//recibe los datos en formato json
            .then((datosrepuesta) => {
-           // console.log('Datos',datosrepuesta.data)//Muestra el resultado de la peticion
+           console.log(datosrepuesta)//Muestra el resultado de la peticion
            })
-           .catch(console.log)//muestra errores
-    
+           .catch(console.log)//muestra errores 
 }
     

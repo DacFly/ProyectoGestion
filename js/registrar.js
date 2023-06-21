72f416e73df735d146d6038dc79fe057423261a8
Jimenez29 <Doryanjimenez2903@hotmail.com>
2023-06-21 16:06:39 -0600

    ss

---
+++ js/registrar.js
@@ -0,0 +1,21 @@
+function crearUsuario() {
+    
+    fetch("https://localhost:7088/Usuarios/CrearUsuario", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(usuario)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Usuario creado exitosamente");
+          // Realizar cualquier acción adicional después de crear el usuario
+        } else {
+          console.error("Error al crear el usuario");
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }

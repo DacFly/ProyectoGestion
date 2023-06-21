e05034d8fc669c08b1a58d004b7b1d8118e8dc9b
Richard Chaves <84647550+DacFly@users.noreply.github.com>
2023-06-21 15:46:29 -0600

    a

--- js/modificarP.js
+++ js/modificarP.js
@@ -1,46 +1,31 @@
-document.querySelector("form").addEventListener("submit", function (e) {
+
+
+
+
+
+
+document.getElementById("modificarP").addEventListener("submit", function (e) {
   e.preventDefault();
+  var usuario = obtenerUsuario();
+  console.log(usuario);
   
-  sessionStorage.getItem("id")
-  var cedula = document.getElementById("cedula").value;
-  var nombre = document.getElementById("user").value;
-  var email = document.getElementById("email").value;
-  var contraseña = document.getElementById("pass").value;
-  var telefono = document.getElementById("telefono").value;
-
-  var datosEnviar = {
-    cedula: cedula,
-    nombre: nombre,
-    email: email,
-    contraseña: contraseña,
-    telefono: telefono
-  };
-
-  fetch("https://localhost:7088/Usuario/EditarUsuario", {
-    method: "PUT",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(datosEnviar)
-  })
-    .then(function (respuesta) {
-      if (respuesta.status == 204) {
-        document.getElementById("cedula").value = "";
-        document.getElementById("user").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("pass").value = "";
-        document.getElementById("telefono").value = "";
-
-        swal(
-          "Se ha modificado correctamente!",
-          "Presiona el botón!",
-          "success"
-        ).then(function (willDelete) {
-          if (willDelete) {
-            // Realizar acciones adicionales después de la modificación exitosa
-          }
-        });
-      } else {
-        swal("No se ha modificado!", "Presiona el botón!", "error");
-      }
-    })
-    .catch(console.log);
 });
+
+  function obtenerUsuario() {
+
+    var id = parseInt(sessionStorage.getItem("id"));
+   
+    fetch("https://localhost:7088/Usuarios/BuscarUsuario/" + id, {
+      method: 'GET'
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error al obtener el usuario");
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }

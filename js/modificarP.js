7befd9cefed102427101c25918a652533d4cec42
Richard Chaves <84647550+DacFly@users.noreply.github.com>
2023-06-21 15:30:34 -0600

    a

--- js/modificarP.js
+++ js/modificarP.js
@@ -1,44 +1,46 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+  sessionStorage.getItem("id")
+  var cedula = document.getElementById("cedula").value;
+  var nombre = document.getElementById("user").value;
+  var email = document.getElementById("email").value;
+  var contraseña = document.getElementById("pass").value;
+  var telefono = document.getElementById("telefono").value;
 
+  var datosEnviar = {
+    cedula: cedula,
+    nombre: nombre,
+    email: email,
+    contraseña: contraseña,
+    telefono: telefono
+  };
 
-var contenidoTablaResultado = document.querySelector("#resultados");
-function cargarEventos2() {
-  console.log(sessionStorage.getItem("id"));
-  fetch(
-    "https://localhost:7088/Usuario/BuscarUsuario/" +
-      sessionStorage.getItem("id"),
-    {
-      method: "GET",
-    }
-  )
-    .then((response) => response.json())
-    .then((datos) => {
-      contenidoTablaResultado.innerHTML = ``;
-      for (const valor of datos) {
-        contenidoTablaResultado.innerHTML += `
-            <tr class="table-primary" >
-                <td>${valor.usuarioId}</td>
-                <td>${valor.cedula}</td>
-                <td>${valor.nombre}</td>
-                <td>${valor.email}</td>
-                <td>${valor.password}</td>
-                <td>${valor.telefono}</td>
-                <td>${valor.rol}</td>
-               
-                <td>
-                  <a name="" id="" class="btn btn-primary" onclick="editar(${sessionStorage.getItem(
-                    "id"
-                  )},'${valor.idEvento}', '${valor.titulo}', '${
-          valor.fechaPublicacion
-        }', '${valor.fechaEvento}', '${valor.hora}', '${valor.descripcion}', '${
-          valor.direccion
-        }')" role="button">Editar</a>
-                </td>
-            </tr>`;
+  fetch("https://localhost:7088/Usuario/EditarUsuario", {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(datosEnviar)
+  })
+    .then(function (respuesta) {
+      if (respuesta.status == 204) {
+        document.getElementById("cedula").value = "";
+        document.getElementById("user").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("pass").value = "";
+        document.getElementById("telefono").value = "";
+
+        swal(
+          "Se ha modificado correctamente!",
+          "Presiona el botón!",
+          "success"
+        ).then(function (willDelete) {
+          if (willDelete) {
+            // Realizar acciones adicionales después de la modificación exitosa
+          }
+        });
+      } else {
+        swal("No se ha modificado!", "Presiona el botón!", "error");
       }
     })
-    .catch((error) => {
-      // Aquí puedes manejar los errores de la solicitud
-      console.error(error);
-    });
-}
-cargarEventos2();
+    .catch(console.log);
+});
